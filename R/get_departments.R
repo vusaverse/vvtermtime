962ce0a890a8dd5687fb6153aa2a19afb843073b
Tomer Iwan <iwantomer@hotmail.com>
2023-07-05 11:34:01 +0200

    feat: get courses and departments

---
+++ R/get_departments.R
@@ -0,0 +1,29 @@
+#' Retrieve department data from the Semestry API
+#'
+#' This function sends a GET request to the departments endpoint of the Semestry API and retrieves the department data.
+#'
+#' @param semestry An authenticated Semestry object.
+#' @param timeout The timeout duration for the GET request (default: 30 seconds).
+#'
+#' @return The retrieved department data from the API.
+#'
+#' @export
+get_departments <- function(semestry, timeout = 30) {
+  endpoint <- "/v1/api/departments"
+
+  url <- paste0(semestry$base_url, endpoint)
+
+  resp <- httr::GET(
+    url,
+    httr::add_headers("Authorization" = paste0("k", semestry$api_key)),
+    httr::timeout(timeout)
+  )
+
+  if (resp$status_code == 200) {
+    content <- httr::content(resp, as = "text", encoding = "UTF-8")
+    data <- jsonlite::fromJSON(content)
+    return(data)
+  } else {
+    stop(paste("Error: Failed to retrieve department data from the API. Status code:", httr::http_status(resp)$status_code))
+  }
+}
