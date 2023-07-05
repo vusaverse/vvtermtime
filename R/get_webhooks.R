8d312e902929c9b8428834d0d6954652043f06fa
Tomer Iwan <iwantomer@hotmail.com>
2023-07-05 11:11:02 +0200

    feat: get webhooks

---
+++ R/get_webhooks.R
@@ -0,0 +1,29 @@
+#' Retrieve webhook data from the Semestry API
+#'
+#' This function sends a GET request to the webhooks endpoint of the Semestry API and retrieves the webhook data.
+#'
+#' @param semestry An authenticated Semestry object.
+#' @param timeout The timeout duration for the GET request (default: 30 seconds).
+#'
+#' @return The retrieved webhook data from the API.
+#'
+#' @export
+get_webhooks <- function(semestry, timeout = 30) {
+  endpoint <- "/v1/api/webhooks"
+
+  url <- paste0(semestry$base_url, endpoint)
+
+  resp <- httr::GET(
+    url,
+    httr::add_headers("Authorization" = paste0("k", semestry$api_key)),
+    httr::timeout(timeout)
+  )
+
+  if (resp$status_code == 200) {
+    content <- httr::content(resp, as = "text", encoding = "UTF-8")
+    data <- jsonlite::fromJSON(content)
+    return(data)
+  } else {
+    stop(paste("Error: Failed to retrieve webhook data from the API. Status code:", httr::http_status(resp)$status_code))
+  }
+}
